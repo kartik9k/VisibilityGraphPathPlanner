79c194033f275ecd23defcb722a9512ce455dbc5
kartik9k <kartik9k@gmail.com>
2018-02-11 21:10:10 +0530

    From lab

--- visibility.py
+++ visibility.py
@@ -1,7 +1,8 @@
 import numpy as np
+import math
 
 class Point:
-	def __init__(self,x,y):
+	def __init__(self,x, y):
 		self.x = x
 		self.y = y
 
@@ -22,7 +23,7 @@ class VisibilityGraph():
 		for i in range(len(self.vertices)):
 			for j in range(len(self.vertices)):
 				if i != j:
-					if self.isVisible(i, j):
+					if self.isVisible(i, j) and self.notInObstacle(i, j):
 						self.visibleVertex.append([self.vertices[i], self.vertices[j]])
 
 		return self.visibleVertex
@@ -31,7 +32,7 @@ class VisibilityGraph():
 		a = Point(self.vertices[i][0], self.vertices[i][1])
 		b = Point(self.vertices[j][0], self.vertices[j][1])
 
-		for (x1, y1, x2, y2) in self.obstacleList:
+		for (x1, y1, x2, y2) in self.edges:
 			c = Point(x1, y1)
 			d = Point(x2, y2)
 
@@ -39,4 +40,30 @@ class VisibilityGraph():
 				return False
 			else:
 				continue
+		return True
+
+	def outSide(self, x, y):
+		for (ox, oy, a, b) in self.obstacleList:
+			if (abs(x - ox) <= a/2 and abs(y - oy) <= b/2):
+				return False
+
+		return True
+
+	def notInObstacle(self, i, j):
+		expandDist = 0.3
+		ax, ay = self.vertices[i]
+		bx, by = self.vertices[j]
+
+		theta = math.atan2(by - ay, bx - ax)
+		xCurr, yCurr = ax, ay
+
+		d = math.sqrt((by - ay) ** 2 + (bx - ax) ** 2)
+		iterations = int(d/expandDist)
+
+		for i in range(iterations):
+			if not self.outSide(xCurr, yCurr):
+				return False
+			xCurr += expandDist * math.cos(theta)
+			yCurr += expandDist * math.sin(theta)
+
 		return True
